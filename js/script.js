00c394ecaefb902051f3c04b7f8dd85400aafdbe
Subhodeep <subhodeeppal64@gmail.com>
2021-12-29 12:23:24 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("get02").onclick = function () {
+    location.href = "article.html"
+};
+
+document.getElementById("get01").onclick = function () {
+    location.href = "explore.html"
+};
